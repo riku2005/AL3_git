daa74fb33c756146a7a704f5300e1f1cffe7559e
riku2005 <149132633+riku2005@users.noreply.github.com>
2024-06-11 14:02:37 +0900

    スライド13枚目まで

---
+++ DirectXGame/MapChipField.cpp
@@ -0,0 +1,18 @@
+﻿#include <map>
+#include "MapChipField.h"
+
+namespace {
+
+	std::map<std::strong_ordering, MapChipType> mapChipTable = {
+		{"0",MapChipType::kBlank},
+		{"1",MapChipType::kBlock},
+	};
+
+}
+
+class ResetMapChipData {
+
+//マップチップデータをリセット
+	mapChipData_
+
+};
---
+++ DirectXGame/MapChipField.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "Model.h"
+#include "WorldTransform.h"
+
+
+enum class MapChipType {
+	kBlank, //空白
+	kBlock, //ブロック
+};
+
+struct MapChipData {
+	std::vector<std::vector<MapChipType>> data;
+};
+
+class MapChipField {
+
+public:
+
+//1ブロックのサイズ
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
+//ブロックの個数
+	static inline const uint32_t kNumBlockVirtical = 20;
+	static inline const uint32_t kNumBlockHorizontal = 100;
+
+	void ResetMapChipData();
+
+
+private:
+
+	MapChipData mapChipData_;
+
+};
