574c2800a72fe9f75115ce2e66720f1a6d9b5d85
riku2005 <149132633+riku2005@users.noreply.github.com>
2024-07-02 14:27:28 +0900

    2-4の内容OK

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -11,6 +11,7 @@
 #include "Player.h"
 #include "DebugCamera.h"
 #include "Skydome.h"
+#include "MapChipField.h"
 
 #include <vector>
 
@@ -45,6 +46,8 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+	void GenerateBlocks();
+
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
@@ -71,6 +74,9 @@ private: // メンバ変数
 	Skydome* skydome_ = nullptr;
 
 	Model* modelSkydome_ = nullptr;
+
+	//マップチップフィールド
+	MapChipField* mapChipField_;
 
 	/// <summary>
 	/// ゲームシーン用
