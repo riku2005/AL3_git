b60b6dd53216cdebee6ed0745d1265108a8a0928
riku2005 <149132633+riku2005@users.noreply.github.com>
2024-07-04 16:56:19 +0900

    スライド28枚目まで

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -39,6 +39,15 @@ void Player::Update() {
 
 		acceleration.x += kAcceleration;
 
+		if (lrDirection_ != LRDirection::kRight) {
+			lrDirection_ = LRDirection::kRight;
+
+			//旋回開始時の角度
+			turnFirstRotationY_ = worldTransform_.rotation_.y;
+			//旋回タイマー
+			turnTimer_ = 0.7f;
+		}
+
 	}
 	else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
 
@@ -50,6 +59,15 @@ void Player::Update() {
 
 		acceleration.x -= kAcceleration;
 
+		if (lrDirection_ != LRDirection::kLeft) {
+			lrDirection_ = LRDirection::kLeft;
+
+			//旋回開始時の角度
+			turnFirstRotationY_ = worldTransform_.rotation_.y;
+			//旋回タイマー
+			turnTimer_ = 0.7f;
+		}
+
 	}
 		
 	//加速/減速
@@ -57,15 +75,38 @@ void Player::Update() {
 	velocity_.y += acceleration.y;
 	velocity_.z += acceleration.z;
 
+	//最大速度制限
 	velocity_.x = std::clamp(velocity_.x,-kLimitRunSpeed,kLimitRunSpeed);
+	
+	if (acceleration.x >= 0.01f || acceleration.x <= -0.01f) {
+		acceleration.x = 0;
 	}
-	else {
+
+	if (turnTimer_ > 0.0f) {
+		//タイマーのカウントダウン
+		turnTimer_ -= 1.0f / 60.0f;
+	}
+
+		//左右の自キャラ角度テーブル
+	float destinationRotationYTable[] = {
+		std::numbers::pi_v<float> / 2.0f,
+		std::numbers::pi_v<float> * 3.0f / 2.0f
+		};
+		//状態に応じた角度を取得する
+		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+		//自キャラの角度を設定する
+		worldTransform_.rotation_.y = destinationRotationY;
+	}else {
 		velocity_.x *= (1.0f - kAttenuation);
 	}
 
 	worldTransform_.translation_.x += velocity_.x;
 	worldTransform_.translation_.y += velocity_.y;
 	worldTransform_.translation_.z += velocity_.z;
+
+	if (onGround_) {
+
+	}
 
 	worldTransform_. TransferMatrix();
 	worldTransform_.UpdateMatrix();
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -9,6 +9,12 @@ class Player {
 
 public:
 
+	//左右
+	enum class LRDirection {
+		kRight,
+		kLeft,
+	};
+
 	void Initialize(ViewProjection*viewProjection,const Vector3& position);
 
 	void Update();
@@ -22,6 +28,29 @@ private:
 	static inline const float kLimitRunSpeed = 2.0f;
 
 	Vector3 velocity_{};
+
+	LRDirection lrDirection_ = LRDirection::kRight;
+
+	//旋回開始時の角度
+	float turnFirstRotationY_ = 0.0f;
+
+	//旋回タイマー
+	float turnTimer_ = 0.0f;
+
+	//旋回時間<秒>
+	static inline const float kTimeTurn = 0.3f;
+
+	//接地状態フラグ
+	bool onGround_ = true;
+	//着地フラグ
+	//bool landing = false;
+
+	//重力加速度(下方向)
+	static inline const float kGravityAcceleration = 0.05f;
+	//最大落下速度(下方向)
+	static inline const float kLimitFallSpeed = 0.2f;
+	//ジャンプ初速(上方向)
+	static inline const float kJumpAcceleration = 0.7f;
 
 	//ワールド変換データ
 	WorldTransform worldTransform_;
